78f8ed2bbf4d8ec8a3ea48bb6a6f44de724923db
Hankeamanda <hanke.amanda@escola.pr.gov.br>
2025-08-07 20:52:35 -0300

    P

---
+++ script.js
@@ -0,0 +1,14 @@
+aCartao(
+    'Quem sou eu?
+    'Meu nome é Amanda,tenho 15 anos'
+)
+
+criaCartao(
+    'Meus hobbies?'
+    ' Tocar guitarra e violão, ler e pintar'
+)
+
+criaCartao(
+    'Meu presente?'
+    'Atualmente estou estudando idiomas e fazendo cursos online'
+)
